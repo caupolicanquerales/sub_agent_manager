6c1f5be75a477defb959111d576153f028146a4a
caupolicanquerales <caupolicanquerales@gmail.com>
2026-05-15 18:16:28 -0300

    Prueba

--- src/main/java/com/capo/sub_agent_manager/SubAgentManagerApplication.java
+++ src/main/java/com/capo/sub_agent_manager/SubAgentManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SubAgentManagerApplication {
 
+	//Prueba
 	public static void main(String[] args) {
 		SpringApplication.run(SubAgentManagerApplication.class, args);
 	}
